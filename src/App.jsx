6a65e52f5f0d13c273ad3ee5fe19e8f5f526d115
shahinrea <shahinrea@gmail.com>
2024-10-17 22:49:05 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
       <TopHeader />
       <Navbar />
       <MenuBar />
+      <div className="max-w-7xl mx-auto flex mt-6">
+        {/* left side */}
+        <div className="w-8/12 h-[250px] border-2 border-red-400"></div>
+        {/* right side */}
+        <div className="w-4/12 h-[250px] border-2 border-blue-300"></div>
+      </div>
     </div>
   );
 }
--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -3,7 +3,7 @@ import menuData from './constant/menuData';
 
 const MenuBar = () => {
   return (
-    <div className="max-w-7xl mx-auto flex flex-row items-center justify-between bg-gray-100 p-2 mt-4 font-notobangla">
+    <div className="max-w-7xl mx-auto flex flex-row items-center hover:bg-blue-200 cursor-pointer justify-between bg-gray-100 p-2 mt-4 font-notobangla">
       {menuData.map((menu) => (
         <div className="" key={menu.id}>
           {menu.item}
